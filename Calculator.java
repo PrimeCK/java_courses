f8b3ac374fac594d97d8dc9bbbedfdc4dea320e1
Andrew Shchurovskiy <schurovskii@gmail.com>
2016-11-17 20:07:05 +0200

    new calc

--- Calculator.java
+++ Calculator.java
@@ -15,6 +15,54 @@ public class Calculator{
     }
 
 
+     /**
+        Multiplaying arguments
+        @param params - Arguments for mult
+    */
+    public void mult(int ... params){
+        
+        this.result = params[0];
+        
+        this.result = this.result==0?1:this.result;
+        
+        for (int i = 1;i<params.length;i++){
+            this.result *=params[i];
+        }
+    }
+
+    
+     /**
+        div arguments
+        @param params - Arguments fo division
+    */
+    public void div(int ... params){
+        
+        this.result = params[0];
+        
+        for (int i = 1;i<params.length;i++){
+            if (params[i] == 0){
+                System.out.println("division by zero. Next step;");
+            }
+            else{
+                this.result /=params[i];    
+            }
+        }
+    }
+
+    
+     /**
+        subtract arguments
+        @param params - Arguments for subtract
+    */
+    public void sub(int ... params){
+    
+        for (int param : params){
+            this.result -=param;
+        }
+    }
+
+    
+    
     public int getResult(){
         return this.result;
     }
--- InterraptRunner.java
+++ InterraptRunner.java
@@ -12,23 +12,71 @@ public class InterraptRunner{
         
         try{
             Calculator  calc = new Calculator();
-            String exit = "no";
-            while(!exit.equals("yes")){
+            String operation = "0";
+            while(!operation.equals("5")){
+                
+                printMenu();
+                
+                operation = reader.next();
+                
                 System.out.println("Input first agument");
-                String first = reader.next();
+                String firstArg = reader.next();
                 System.out.println("Input second agument");
-                String second = reader.next();
-                calc.add(Integer.valueOf(first),Integer.parseInt(second));
+                String secondArg = reader.next();
+                
+                doOperation(calc,operation,Integer.parseInt(firstArg),Integer.parseInt(secondArg));
+                
                 System.out.println("Result: "+calc.getResult());
+                
                 calc.cleanResult();
-                System.out.println("Exit yes/no");
-                exit = reader.next();       
+                
+                System.out.println("Press any key...");
+                
+                reader.next();
+            
             }
         }catch(Exception e){   
             System.out.println(e);    
         }finally{reader.close();
                  System.out.println("Bay");}
     
+    }
+    
+    
+    private static void printMenu(){
+        
+        System.out.println("Enter operation:");
+        System.out.println("1. add");
+        System.out.println("2. mult");
+        System.out.println("3. sub");
+        System.out.println("4. div");
+        System.out.println("5. exit");            
+    }
+    
+    private static void doOperation(Calculator calc,String operation,int firstArg,int secondArg){
+        
+        int presentRes = calc.getResult();
+
+        
+        System.out.println(""+presentRes+"   "+firstArg+"   "+secondArg);  
+        
+        if(operation.equals("1")){
+            calc.add(presentRes,0);
+            presentRes = calc.getResult();  
+            calc.add(firstArg,secondArg);
+        } else if(operation.equals("2")){
+            calc.mult(presentRes,1);
+            presentRes = calc.getResult();   
+            calc.mult(firstArg,secondArg);
+        } else if(operation.equals("3")){
+            calc.sub(firstArg,0);
+            presentRes = calc.getResult();   
+            calc.sub(firstArg,secondArg);
+        } else if(operation.equals("4")){
+            calc.div(firstArg,1);
+            presentRes = calc.getResult();   
+            calc.div(firstArg,secondArg);
+        }
     }
 
 
